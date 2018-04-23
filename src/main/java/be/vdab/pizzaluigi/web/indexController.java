e25cb8e5837c0103ed65bf8f54cfbb2043fdc65c
Stefan Schuddinck <stefanschuddinck@hotmail.com>
2018-04-23 17:49:45 +0200

    met flexibele begroeting en jsp

--- src/main/java/be/vdab/pizzaluigi/web/indexController.java
+++ src/main/java/be/vdab/pizzaluigi/web/indexController.java
@@ -2,22 +2,27 @@ package be.vdab.pizzaluigi.web;
 
 import java.time.LocalTime;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController 
+@Controller
 @RequestMapping("/")
 class indexController {
-	 @GetMapping 
-	 String index() { 
-		 int uur = LocalTime.now().getHour();  
-		 if (uur < 12) {   
-			 return "Goede morgen"; 
-			 }     
-		 if (uur < 18) {  
-			 return "Goede middag";  
-			 }    
-		 return "Goede avond";  
-		 }
+	@GetMapping
+	ModelAndView index() {
+		  String boodschap;
+		int uur = LocalTime.now().getHour();
+		if (uur < 12) {
+			boodschap = "Goede morgen";
+		} else if (uur < 17) {
+			boodschap = "Goede middag";
+		} else {
+			boodschap = "Goede avond";
+		}
+
+		  return new ModelAndView("/WEB-INF/JSP/index.jsp", "boodschap", boodschap);
+
+	}
 }
---
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<title>Pizza Luigi</title>
+<link rel='icon' href='images/pizza.ico' type='image/x-icon'>
+<meta name='viewport' content='width=device-width,initial-scale=1'>
+<link rel='stylesheet' href='css/pizzaluigi.css'>
+</head>
+<body>
+	<h1>Pizza Luigi</h1>
+	<img src='images/pizza.jpg' alt='pizza' class='fullwidth'>
+	<h2>${boodschap}</h2>
+</body>
+</html>
